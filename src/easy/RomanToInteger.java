247a3a14c321494aa0d656d9d7921fb4d61284e1
ressKim <sh21dlat@gmail.com>
2021-07-30 17:05:33 +0900

    Roman To Integer - 2차 수정 복잡한 쿼리 간단하게 수정

--- src/easy/RomanToInteger.java
+++ src/easy/RomanToInteger.java
@@ -21,31 +21,23 @@ public class RomanToInteger {
         romanMap.put("M", 1000);
 
         int result = 0;
-        int same_value = 0;
+
         String[] split_strings = s.split("");
+
         int[] int_arr = new int[split_strings.length];
+
         for (int i = 0; i < split_strings.length; i++) {
             int_arr[i] =romanMap.get(split_strings[i]);
         }
-        for(int i = 0; i< int_arr.length; i++){
-            if (i == int_arr.length - 1) {
-                result += int_arr[i];
-                result += same_value;
-                break;
-            }
+
+        for(int i = 0; i< int_arr.length-1; i++){
             if (int_arr[i] < int_arr[i + 1]) {
                 result -= int_arr[i];
-                result -= same_value;
-                same_value = 0;
-                continue;
-            } else if (int_arr[i] == int_arr[i + 1]) {
-                same_value += int_arr[i];
                 continue;
             }
             result += int_arr[i];
-            result += same_value;
-            same_value = 0;
         }
+        result += int_arr[split_strings.length-1];
         return result;
     }
 
