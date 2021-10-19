66f525f2f85dcd7381e08eac5203df27a1748ffa
ressKim <sh21dlat@gmail.com>
2021-10-19 19:48:50 +0900

    조이스틱 문제 해결

--- src/programmers/levelTwo/조이스틱.java
+++ src/programmers/levelTwo/조이스틱.java
@@ -52,94 +52,19 @@ public class 조이스틱 {
                 }
             }
 
-            // A가 아닌 값들 좌우로 왔다갔다 하는 수 계산
-            /**
-             * A 가 아닌값들을 A 로 바꾸면서 왔다갔다 계산
-             */
-            if (!nameArray[0].equals("A")) {
-                nameArray[0] = "A";
-                ACount++;
-            }
-            int loc = 0;
-            int rightCount = 0;
-            int rightNextCount = 0;
-            int leftCount = 0;
-            int leftNextCount = 0;
-            int plusLocation = 0;
-            int minusLocation = 0;
-            while (ACount != nameArray.length) {
-                while (true) {
-                    plusLocation = getPlusLocation(loc, rightCount, nameArray.length);
-                    if (nameArray[plusLocation].equals("A")) {
-                        rightCount++;
-                    } else {
-                        break;
-                    }
-                }
-                while (true) {
-                    minusLocation = getMinusLocation(loc, leftCount, nameArray.length);
-                    if (nameArray[minusLocation].equals("A")) {
-                        leftCount++;
-                    } else {
-                        break;
-                    }
-                }
-                if (leftCount < rightCount) {
-                    loc = getMinusLocation(loc, leftCount, nameArray.length);
-                    nameArray[loc] = "A";
-                    answer += leftCount;
-                } else if (leftCount == rightCount) {
-                    while (true) {
-                        plusLocation = getPlusLocation(loc, rightNextCount, nameArray.length);
-                        if (nameArray[plusLocation].equals("A")) {
-                            rightNextCount++;
-                        } else {
-                            break;
-                        }
-                    }
-                    while (true) {
-                        minusLocation = getMinusLocation(loc, leftNextCount, nameArray.length);
-                        if (nameArray[minusLocation].equals("A")) {
-                            leftNextCount++;
-                        } else {
-                            break;
-                        }
-                    }
+            int len = name.length();
+            int min = len - 1;
 
-                    if (leftCount + leftNextCount > rightCount + rightNextCount) {
-                        loc = getPlusLocation(loc, rightCount, nameArray.length);
-                        nameArray[loc] = "A";
-                        answer += rightCount;
-                    }else{
-                        loc = getMinusLocation(loc, leftCount, nameArray.length);
-                        nameArray[loc] = "A";
-                        answer += leftCount;
-                    }
-
-                } else {
-                    loc = getPlusLocation(loc, rightCount, nameArray.length);
-                    nameArray[loc] = "A";
-                    answer += rightCount;
-                }
-                ACount++;
+            for (int i = 0; i < len; i++) {
+                int nextIndex = i + 1;
+                while (nextIndex < len && name.charAt(nextIndex) == 'A')
+                    nextIndex++;
+                min = Math.min(min, (i * 2) + len - nextIndex);
             }
+
+            answer += min;
             return answer;
         }
 
-        private int getPlusLocation(int loc, int rightCount, int length) {
-            if ((loc + rightCount) >= length) {
-                return loc + rightCount - length;
-            } else {
-                return loc + rightCount;
-            }
-        }
-
-        private int getMinusLocation(int loc, int leftCount, int length) {
-            if ((loc - leftCount) < 0) {
-                return length + loc - leftCount;
-            } else {
-                return loc - leftCount;
-            }
-        }
     }
 }
